56fb63f310f00b29b5d259a602bed20c57eb4352
Agustin <agus.lebed@gmail.com>
2023-07-14 04:03:00 -0300

    Los ultimos 4/5 commits son canciones nada mas.

--- juego.py
+++ juego.py
@@ -46,6 +46,7 @@ BLUE = (0,0,255)
 GRAY = (64,64,64)
 PURPLE=(148,0,211)
 
+
 BACKGROUND = pygame.image.load('resources/bg1.jpg').convert()
 PLAYLIST = ["resources/venYSanaMiDolor.mp3",
             "resources/muchachosHomero.mp3",
